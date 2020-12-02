10840ff963441e8c8436159fab5ae19f129496d1
daejo <daejo@icloud.com>
2020-12-02 03:27:53 -0800

    adds bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,9 @@ module.exports = {
           $: "jquery",
           jQuery: "jquery"
         }),
-      ],
+        new BundleAnalyzerPlugin({
+          analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+        })
+      ],    
     mode: 'development'
 };
