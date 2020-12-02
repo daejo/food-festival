62b4a407b455b9ca43a650af6d37fe341cd6c9a6
daejo <daejo@icloud.com>
2020-12-01 23:20:42 -0800

    starts modularizing functions

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const path = require("path");
 const webpack = require("webpack");
 
@@ -7,11 +8,14 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.bundle.js'
     },
-    plugins:[
-        new webpack.ProvidePlugin({
-          $: "jquery",
-          jQuery: "jquery"
-        }),
-      ],
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery"
+      }),
+      new BundleAnalyzerPlugin({
+        analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+      })
+    ],    
     mode: 'development'
 };
